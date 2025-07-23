658bbeb1a18d1c3bf3e6cb52e7a7477a1c28c7d3
yumoqing <yumoqing@gmail.com>
2025-07-23 18:18:02 +0800

    bugfix

--- dapi/init.py
+++ dapi/init.py
@@ -2,7 +2,7 @@ from dapi.dapi import sync_user, bearer_auth, deerer_auth
 from ahserver.serverenv import ServerEnv
 from rbac.check_perm import register_auth_method
 
-def load_kyapikeyserver():
+def load_dapi():
 	env = ServerEnv()
 	env.sync_user = sysnc_user
 	register_auth_method('Bearer', bearer_auth)
