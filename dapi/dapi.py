89a4bdd42f0ca1bf4a9c3c8aff58c808270e8d03
yumoqing <yumoqing@gmail.com>
2025-09-02 13:52:13 +0800

    bugfix

--- dapi/dapi.py
+++ dapi/dapi.py
@@ -28,7 +28,7 @@ async def get_secretkey(sor, appid):
 		return None
 	secretkey = recs[0].secretkey
 	f = get_serverenv('password_decode')
-	return f(secretkey).encode('utf-8')
+	return f(secretkey)
 
 async def get_apikey_user(sor, apikey, client_ip):
 	f = get_serverenv('password_encode')
